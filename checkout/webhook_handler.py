f32ae331392c2a9dfd0440a247ab811e002c1d3d
rlorimier <rere.rnt@gmail.com>
2022-10-16 21:15:08 +0000

    added payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -18,11 +18,13 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
