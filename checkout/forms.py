d516ed5d4842ae98228fddf32877b41eaac57828
rlorimier <rere.rnt@gmail.com>
2022-10-18 17:04:26 +0000

    updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,7 +22,7 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
+            # 'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -33,13 +33,14 @@ class OrderForm(forms.ModelForm):
         #Next we're setting the autofocus attribute on the full name field to true so the cursor will start in the full name field when the user loads the page.
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            #finally we iterate through the forms fields adding a star to the placeholder if it's a required field on the model.
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            #Setting all the placeholder attributes to their values in the dictionary above.
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                #finally we iterate through the forms fields adding a star to the placeholder if it's a required field on the model.
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                #Setting all the placeholder attributes to their values in the dictionary above.
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             #Adding a CSS class we'll use later.
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             #then removing the form fields labels since we won't need them given the placeholders are now set.
