f3744a406d17f1c34b3ca72ce785a562e0051bd3
rlorimier <rere.rnt@gmail.com>
2022-10-18 22:00:07 +0000

    updated views

--- checkout/admin.py
+++ checkout/admin.py
@@ -20,7 +20,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid')
 
     #it will allow us to specify the order of the fields in the admin interface which would otherwise be adjusted by django due to the use of some read-only fields.
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
