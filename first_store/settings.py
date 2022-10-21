4b64822662c6ef32a5f0b5674ac49ef380b9d6f3
rlorimier <rere.rnt@gmail.com>
2022-10-21 18:55:46 +0000

    removed secret key

--- first_store/settings.py
+++ first_store/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=7k6-@ye35%90vu6-2+zm2$6(-g7(8a96x=#$piw6rjf2*ix8-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['my-first-store-app.herokuapp.com', 'localhost']
 
