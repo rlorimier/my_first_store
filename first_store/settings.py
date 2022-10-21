8808eddf1cf0ccfa160e686f01df584a4373ecc5
rlorimier <rere.rnt@gmail.com>
2022-10-21 20:11:15 +0000

    deployment

--- first_store/settings.py
+++ first_store/settings.py
@@ -178,7 +178,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.environ
+if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'my-first-store-app'
     AWS_S3_REGION_NAME = 'EU (Ireland) eu-west-1'
